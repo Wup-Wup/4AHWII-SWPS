91b2dc87ba7c4b1ca7316bebe3e3602df952dee9
Wup-Wup <55537968+Wup-Wup@users.noreply.github.com>
2020-11-20 15:27:30 +0100

    Add files via upload

---
+++ Binärsuche/src/Normalsuche.java
@@ -0,0 +1,13 @@
+public class Normalsuche {
+	
+	static public void nSuche() {
+		
+		 
+		for (int i = 0; i < testmain.array.length; i++) {
+			if(testmain.zahl==testmain.array[i]) {
+				break;
+			}
+			i++;
+		}
+	}
+}
